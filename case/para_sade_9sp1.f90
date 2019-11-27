23faf1da2ff603d6153e0e38c43e800761c4dcc4
richair <richest0320@gmail.com>
2019-11-27 14:15:31 +0800

    update para sade 9sp1 args

--- case/para_sade_9sp1.f90
+++ case/para_sade_9sp1.f90
@@ -1,5 +1,6 @@
 program sade_9sp1
     use mpi_sade
     implicit none
-    call run_parallel_sade("9sp1", 5, 5*5*4*10, 100000, 100.d0, 100, 100, 15, 5, 0.d0)
+    call run_parallel_sade("9sp1", 5, 5*5*4*10, 100000, 100.d0, 100, 100, &
+        15, 12, 0.1d0)
 end program sade_9sp1
